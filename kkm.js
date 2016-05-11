aed00af82bbc1c4908a5ae1664bcce0fc5f36e60
ivanadinata <ivanadinata.s@gmail.com>
2016-05-11 16:41:26 +0700

    versi 1.0

---
+++ kkm.js
@@ -0,0 +1,101 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '',
+	database : 'dbnilai',
+});
+
+module.exports ={	
+	get : function(req,res){
+		var data = {
+			"error":1,
+			"dbnilai":""
+		};
+
+		connection.query("SELECT * from tbl_kkm",function(err, rows, fields){
+			if(rows.length != 0){
+				data["error"] = 0;
+				data["dbnilai"] = rows;
+				res.json(data);
+				console.log(data)
+			}else{
+				data["tbl_kkm"] = 'No kkm Found..';
+				res.json(data);
+			}
+		});
+	},
+
+	post :function(req,res){
+		var id = req.body.id;
+		var kkm = req.body.kkm;
+		var data = {
+			"error":1,
+			"tbl_kkm":""
+		};
+		if(!!id && !!kkm ){
+			connection.query("INSERT INTO tbl_kkm VALUES(?,?)",[id,kkm],function(err, rows, fields){
+				if(!!err){
+					data["tbl_kkm"] = "Error Adding data";
+					console.log(err)
+				}else{
+					data["error"] = 0;
+					data["tbl_kkm"] = "kkm Added Successfully";
+				}
+				res.json(data);
+			});
+		}else{
+			data["tbl_kkm"] = "Please provide all required data (i.e : id, kkm)";
+			res.json(data);
+		}
+	},
+
+	put : function(req,res){
+		var id = req.body.id;
+		var kkm = req.body.kkm;
+		var data = {
+			"error":1,
+			"dbnilai":""
+		};
+		if(!!id && !!kkm ){
+			connection.query("UPDATE tbl_kkm SET kkm=? WHERE id=?",[kkm,id],function(err, rows, fields){
+				if(!!err){
+					data["dbnilai"] = "Error Updating data";
+					console.log(err)
+				}else{
+					data["error"] = 0;
+					data["dbnilai"] = "put kkm Successfully";
+				}
+				res.json(data);
+
+			});
+		}else{
+			data["dbnilai"] = "Please provide all required data (i.e :id,kkm)";
+			res.json(data);
+		}
+	},
+
+	delete : function(req,res){
+		var id = req.body.id;
+		var data = {
+			"error":1,
+			"dbnilai":""
+		};
+		if(!!id){
+			connection.query("DELETE FROM tbl_kkm WHERE id=?",[id],function(err, rows, fields){
+				if(!!err){
+					data["dbnilai"] = "Error deleting data";
+					console.log(err)
+				}else{
+					data["error"] = 0;
+					data["dbnilai"] = "Delete kkm Successfully";
+				}
+				res.json(data)
+			});
+		}else{
+			data["dbnilai"] = "Please provide all required data (i.e : id )";
+			res.json(data);
+		}
+	}
+
+};
---
+++ server.js
@@ -0,0 +1,62 @@
+var app   = require('express')();
+var http = require('http').Server(app);
+var mysql = require('mysql');
+var bodyParser = require("body-parser");
+var connection = mysql.createConnection({
+		host     : 'localhost',
+		user     : 'root',
+		password : '',
+		database : 'dbnilai',
+	});
+
+var usercontroller = require('./user');
+var typecontroller = require('./type');
+var kelascontroller = require('./kelas');
+var statuscontroller = require('./status');
+var kkmcontroller = require('./kkm');
+var tlpcontroller = require('./tlp');
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+	
+	//tbl_user
+	app.get('/tbl_user',usercontroller.get)
+	app.post('/tbl_user',usercontroller.post)
+	app.put('/tbl_user/:id',usercontroller.put)
+	app.delete('/tbl_user/:id',usercontroller.delete)
+
+	//tbl_type
+	app.get('/tbl_type',typecontroller.get)
+	app.post('/tbl_type',typecontroller.post)
+	app.put('/tbl_type/:id',typecontroller.put)
+	app.delete('/tbl_type/:id',typecontroller.delete)
+
+	//tbl_kelas
+	app.get('/tbl_kelas',kelascontroller.get)
+	app.post('/tbl_kelas',kelascontroller.post)
+	app.put('/tbl_kelas/:id',kelascontroller.put)
+	app.delete('/tbl_kelas/:id',kelascontroller.delete)
+
+	//tbl_status
+	app.get('/tbl_status',statuscontroller.get)
+	app.post('/tbl_status',statuscontroller.post)
+	app.put('/tbl_status/:id',statuscontroller.put)
+	app.delete('/tbl_status/:id',statuscontroller.delete)
+
+	//tbl_kkm
+	app.get('/tbl_kkm',kkmcontroller.get)
+	app.post('/tbl_kkm',kkmcontroller.post)
+	app.put('/tbl_kkm/:id',kkmcontroller.put)
+	app.delete('/tbl_kkm/:id',kkmcontroller.delete)
+
+	//tbl_tlp
+	app.get('/tbl_tlp',tlpcontroller.get)
+	app.post('/tbl_tlp',tlpcontroller.post)
+	app.put('/tbl_tlp/:id',tlpcontroller.put)
+	app.delete('/tbl_tlp/:id',tlpcontroller.delete)
+
+http.listen(8080,function(){
+	console.log("Connected & Listen to port 8080");
+});
